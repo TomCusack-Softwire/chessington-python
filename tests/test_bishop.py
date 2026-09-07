cb5d18849bf6bd1136ecec70f77c8c07768a7447
TomCusack-Softwire <Tom.Cusack@softwire.com>
2026-09-07 14:49:08 +0100

    Add tests for bishops moving through other pieces

--- tests/test_bishop.py
+++ tests/test_bishop.py
@@ -29,3 +29,39 @@ def test_bishop_can_move_diagonally():
     ]
     assert len(moves) == len(expected_moves)
     assert set(moves) == set(expected_moves)
+
+
+def test_bishop_is_blocked_by_enemy_pieces():
+    # Arrange
+    board = Board.empty()
+    bishop = Bishop(Player.WHITE)
+    square = Square.at(3, 5)
+    board.set_piece(square, bishop)
+
+    enemy = Pawn(Player.BLACK)
+    enemy_square = Square.at(5, 3)
+    board.set_piece(enemy_square, enemy)
+
+    # Act
+    moves = bishop.get_available_moves(board)
+
+    # Assert
+    assert Square.at(6, 2) not in moves
+
+
+def test_bishop_is_blocked_by_friendly_pieces():
+    # Arrange
+    board = Board.empty()
+    bishop = Bishop(Player.WHITE)
+    square = Square.at(3, 5)
+    board.set_piece(square, bishop)
+
+    friendly = Pawn(Player.WHITE)
+    friendly_square = Square.at(5, 3)
+    board.set_piece(friendly_square, friendly)
+
+    # Act
+    moves = bishop.get_available_moves(board)
+
+    # Assert
+    assert Square.at(6, 2) not in moves
