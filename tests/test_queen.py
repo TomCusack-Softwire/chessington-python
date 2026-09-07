8e14d6c8d3010738028452822ffcf59d8a2e5143
TomCusack-Softwire <Tom.Cusack@softwire.com>
2026-09-07 14:50:22 +0100

    Add tests for queens moving through other pieces

--- tests/test_queen.py
+++ tests/test_queen.py
@@ -45,3 +45,39 @@ def test_queen_can_move_diagonally_and_laterally():
     ]
     assert len(moves) == len(expected_moves)
     assert set(moves) == set(expected_moves)
+
+
+def test_queen_is_blocked_by_enemy_pieces():
+    # Arrange
+    board = Board.empty()
+    queen = Queen(Player.WHITE)
+    square = Square.at(4, 4)
+    board.set_piece(square, queen)
+
+    enemy = Pawn(Player.BLACK)
+    enemy_square = Square.at(6, 6)
+    board.set_piece(enemy_square, enemy)
+
+    # Act
+    moves = queen.get_available_moves(board)
+
+    # Assert
+    assert Square.at(7, 7) not in moves
+
+
+def test_queen_is_blocked_by_friendly_pieces():
+    # Arrange
+    board = Board.empty()
+    queen = Queen(Player.WHITE)
+    square = Square.at(4, 4)
+    board.set_piece(square, queen)
+
+    friendly = Pawn(Player.WHITE)
+    friendly_square = Square.at(6, 6)
+    board.set_piece(friendly_square, friendly)
+
+    # Act
+    moves = queen.get_available_moves(board)
+
+    # Assert
+    assert Square.at(7, 7) not in moves
