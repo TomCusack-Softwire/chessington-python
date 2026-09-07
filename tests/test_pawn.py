0a4b8930aba61564d80fc5e824dd1e431b1afa82
TomCusack-Softwire <Tom.Cusack@softwire.com>
2026-09-07 14:39:41 +0100

    Add tests for pawns moving two spaces initially

--- tests/test_pawn.py
+++ tests/test_pawn.py
@@ -29,3 +29,65 @@ def test_black_pawns_can_move_down_one_square():
 
     # Assert
     assert Square.at(5, 4) in moves
+
+
+def test_white_pawn_can_move_up_two_squares_if_not_moved():
+    # Arrange
+    board = Board.empty()
+    pawn = Pawn(Player.WHITE)
+    square = Square.at(1, 4)
+    board.set_piece(square, pawn)
+
+    # Act
+    moves = pawn.get_available_moves(board)
+
+    # Assert
+    assert Square.at(3, 4) in moves
+
+
+def test_black_pawn_can_move_down_two_squares_if_not_moved():
+    # Arrange
+    board = Board.empty()
+    pawn = Pawn(Player.BLACK)
+    square = Square.at(6, 4)
+    board.set_piece(square, pawn)
+
+    # Act
+    moves = pawn.get_available_moves(board)
+
+    # Assert
+    assert Square.at(4, 4) in moves
+
+
+def test_white_pawn_cannot_move_up_two_squares_if_already_moved():
+    # Arrange
+    board = Board.empty()
+    pawn = Pawn(Player.WHITE)
+    starting_square = Square.at(1, 4)
+    board.set_piece(starting_square, pawn)
+
+    intermediate_square = Square.at(2, 4)
+    pawn.move_to(board, intermediate_square)
+
+    # Act
+    moves = pawn.get_available_moves(board)
+
+    # Assert
+    assert Square.at(4, 4) not in moves
+
+
+def test_black_pawn_cannot_move_down_two_squares_if_already_moved():
+    # Arrange
+    board = Board.empty()
+    pawn = Pawn(Player.BLACK)
+    starting_square = Square.at(6, 4)
+    board.set_piece(starting_square, pawn)
+
+    intermediate_square = Square.at(5, 4)
+    pawn.move_to(board, intermediate_square)
+
+    # Act
+    moves = pawn.get_available_moves(board)
+
+    # Assert
+    assert Square.at(3, 4) not in moves
